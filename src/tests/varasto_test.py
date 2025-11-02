e9e3a6112c33b96b6db75e28f237ad4759a053db
panjuhle <panu.lepp@gmail.com>
2025-11-02 18:25:46 +0200

    Poetry toimii, tehty tehtävän 8 coverage 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,44 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+
+    def test_init_tilavuus_arvolla_joka_pienempi_kuin_0(self):
+        varasto = Varasto(-1,0)
+        self.assertAlmostEqual(varasto.tilavuus, 0)
+
+    def test_init_virheellinen_alkusaldo(self):
+        varasto = Varasto(2,-1)
+
+
+    def test_lisaa_varastoon_negatiivisella_arvolla(self):
+        self.varasto.lisaa_varastoon(-1)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_lisaa_varastoon_maara_on_pienempi_tai_yhtasuuri_kuin_paljonko_mahtuu(self):
+        self.varasto.lisaa_varastoon(5)
+
+        self.assertAlmostEqual(self.varasto.saldo, 5)
+
+
+    def test_lisaa_varastoon_maara_on_suurempi_kuin_paljonko_mahtuu(self):
+        self.varasto.lisaa_varastoon(15)
+
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+
+    def test_ota_varastosta_maara_alla_nolla(self):
+        self.varasto.lisaa_varastoon(5)
+        self.varasto.ota_varastosta(-1)
+
+        self.assertAlmostEqual(self.varasto.saldo, 5)
+
+    
+    def test_ota_varastosta_kaikki(self):
+        self.varasto.lisaa_varastoon(5)
+        self.varasto.ota_varastosta(10)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_olio_tekstiksi(self):
+        self.assertAlmostEqual(str(self.varasto), f"saldo = {self.varasto.saldo}, vielä tilaa {self.varasto.paljonko_mahtuu()}")
